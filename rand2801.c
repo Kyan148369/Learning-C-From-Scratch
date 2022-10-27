73a5732723d29834d84c503a1bcf869a2b083254
Kyan148369 <kyanssharma10@gmail.com>
2022-10-28 02:45:07 +0530

    String literal read only and modifiable character array

---
+++ rand2801.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    char* string1 = "Curious and curiouser";
+    char string2[] = "Lmao";
+
+    scanf("string2[] = %s\n", string2);
+    printf( "string2[] = %s\n", string2);
+    printf( "string1[] = %c\n", string1[2]);
+// doubt in this prog although Im inputting and storing the value of string2 isnt updating
+}
