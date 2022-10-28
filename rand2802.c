332121d304a9985e5f6f056e887172b189e9b5f7
Kyan148369 <kyanssharma10@gmail.com>
2022-10-28 13:46:50 +0530

    address does not change on changing value in pointer

--- rand2802.c
+++ rand2802.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-#include<stdlib.h>
 //Playing around with consts over here 
 int main(){
 const int var = 10;
-
 int *ptr = &var; // initializing pointer as the value stored in address of var
+printf("ptr = %p\n", &var);
+
 *ptr = 12; //assign 12 in pointer
 
 printf("var = %d\n", var);
 printf("*ptr = %d\n", *ptr);
+printf("ptr = %p\n", &var);
 return 0;  
 }
