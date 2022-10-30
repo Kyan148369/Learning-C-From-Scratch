4f6869afbedce53f545c9fb6a0f29614790247e9
Kyan148369 <kyanssharma10@gmail.com>
2022-10-30 14:35:05 +0530

    Completed Structs

--- Structs3001.c
+++ Structs3001.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 struct Student{
         int rollnumber;
-        int grades;
+        int grade;
     };
 int main() {
     struct Student Student1;
         Student1.rollnumber = 13;
-        Student1.grades = 89;
+        Student1.grade = 89;
+      struct Student* ptr = malloc(sizeof(struct Student));
+    ptr->rollnumber = 17;
+    ptr->grade = 76;
+
+    printf("using direct ref to the structure: %d\n", Student1.grade);
+
+    printf("Using a pointer to this structure: %d\n", ptr->grade);
+    printf("using a pointer to the structure: %d\n", (*ptr).grade);   
    
 
     return 0;
