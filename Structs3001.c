92e127edccdc98726745f38bb9478d8567647e6a
Kyan148369 <kyanssharma10@gmail.com>
2022-10-30 14:31:35 +0530

    Started playing around with structs

---
+++ Structs3001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Student{
+        int rollnumber;
+        int grades;
+    };
+int main() {
+    struct Student Student1;
+        Student1.rollnumber = 13;
+        Student1.grades = 89;
+   
+
+    return 0;
+}
