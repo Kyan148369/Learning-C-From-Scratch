609c9f11024dbe15d089667b9957108e7edae07c
Kyan148369 <kyanssharma10@gmail.com>
2022-11-01 14:16:45 +0530

    Progress

--- Deliverables/LinkedList3001.c
+++ Deliverables/LinkedList3001.c
@@ -6,32 +6,35 @@
 // };
 int main(){ 
     int n;
-    printf("Enter size of linked list"); 
+    printf("Enter no of elements of linked list"); 
     scanf("%d\n", &n); //entering number of elements in linked list
   ----------------------------------------------------------------------  
-    while (n > 1){  //Taking the value for each element of the linked list
-        int i = 1;
-        int j =0;
-        if (n>i){
-            printf("Enter value of the %d variable "i );
-            scanf ("%d", &j); 
-            n_i.val = j;
-            i++;
+    while(n > 1){  //Taking the value for each element of the linked list
+        int i = 1; // counter over here to take the variables
+        int j =0; // using this to store the ith value entered in the list
+             if (n>i){
+                printf("Enter value of the %d variable "i ); //
+                 scanf ("%d", &j); 
+                 struct node n_i; // Defining the struct type
+                 n_i.val = j; //assigning value to the node
+                n_i.next = &n_(i+1); //adding the address of the next node to it
+                 i++;
+            }
+            else if (n = i) {
+                printf("Enter value of the %d variable "i );
+                scanf ("%d", &j); 
+                struct node n_i;
+                n_i.val = j;   
+                n_i.next = NULL;
+            }
+            if (i< n; i =0, i++) {
+             printf("node n_i is: %d\n",n_i.val); 
+             printf("node n_2 is: %p\n", n_i.next);
+            }
         }
-        
-    }
-    // struct node n_1, n_2; //defining variables in the struct type
-    // //struct node* ptr = malloc(sizeof(struct node));
-    // //scanf("n1_val = %d\n", n_1.val);
-    //  n_1.val = 1; //assigning values to the variables
-    //  n_2.val = 4; // ""
-    // n_1.next = &n_2; // Assigning the node part of a linked list as a pointer to the address of the n_2 variable over here
-    // n_2.next = NULL; // Assigning null to the pointer here 
-    // printf("node n_1 is: %d\n",n_1.val); 
-    // printf("node n_2 is: %d\n",n_2.val);
-    // printf("node n_2 is: %p\n", n_1.next);
-    // printf("node n_1 address is: %p\n",&n_1.val ); //printing out the address of n_1.val just to see 
-    return 0;
+        return 0;
 }
 
-
+    
+    
+  
