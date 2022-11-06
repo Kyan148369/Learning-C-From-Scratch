0d7f7f1bd2d1b6a195b7a86b2ea5a57bc099f27b
Kyan148369 <kyanssharma10@gmail.com>
2022-11-07 04:28:04 +0530

    Finished linkedlist need to debug 1 error

--- Deliverables/LinkedList3001.c
+++ Deliverables/LinkedList3001.c
@@ -1,51 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node{ // creating struct with vals and nodes
-        int val; //value stored at a node
-        struct node* next; //Pointer(points to the address) of the next node 
-}*stdnode; //global node type to reference any randomn node
-void ElementsinList(int n); //Creating Function to get all the elemtns of the list
-void Displaylist(); // Creating function to display the list we got 
+struct node{                                                      // creating struct with vals and nodes
+        int val;                                                 //value stored at a node
+        struct node *next;                                       //Pointer(points to the address) of the next node 
+}*stdnode;                                                      //global node type to reference any randomn node
+void ElementsinList(int n);                                     //Creating Function to get all the elemtns of the list
+void displaylist();                                             // Creating function to display the list we got 
 int main(){ 
-    int n; // initializing input
+    int n;                                                      //initializing input
     printf("Enter no of elements of linked list"); 
-    scanf("%d\n", &n); //entering number of elements in linked list
-    ElementsinList(n); //Taking the function that takes all the elements we want in the list with their values
+    scanf("%d\n", &n);                                         //entering number of elements in linked list
+    ElementsinList(n);                                         //Taking the function that takes all the elements we want in the list with their values
     printf("Data entered in the list is");
-    Displaylist(); //Displays the entire list
-    return 0;}
+    displaylist();                                            //Displays the entire list
+    return 0;
+    }
   //  ----------------------------------------------------------------------  
-   void ElementsInList(int n) {
-    while(n > 1){  //Taking the value for each element of the linked list
-        int i = 1; // counter over here to take the variables
-        int j = 0; // using this to store the ith value entered in the list
-             if (n>i){
-                printf("Enter value of the %d variable ",i ); //
-                 scanf ("%d", &j); 
-                 struct node n_i; // Defining the struct type
-                 n_i.val = j; //assigning value to the node
-                n_i.next = &n_(i+1); //adding the address of the next node to it
-                 i++;
+    void ElementsInList(int n) {
+    struct node *funcnode,*temp;                              // creating pointers in the linked list
+    int val, i;                                                      // gotta reinitiliaze variables inside func
+                                                     
+    stdnode = (struct node* ) malloc (sizeof(struct node));    // Allocating memory of nodes based on number of nodes 
+                
+    
+    if(stdnode == NULL )  
+     {                               //check whether the standard node is NULL and if so no memory allocation
+         print ("Memory wont be allocated");
+    }
+    else {
+
+        printf("Enter first element of list");               //entering the head element of the linked list
+        scanf("%d\n",&val) ; 
+        stdnode->val = val;                                 // assigning the pointer of the val to the val entered at address of n 
+                                                            // equivalent of saying head.val = val;
+        stdnode->next = NULL;                               // assigning the value of this node to NULL;
+        temp = stdnode;                                     // Creating temp as the stdnode for the head now
+
+
+        for (i=2;i<=n; i++) 
+        {
+                funcnode = (struct node*) malloc (sizeof(struct node)); //Defining node type for remaining nodes
+            
+            if (funcnode = NULL) 
+            {
+ 
+                printf(" Memory wont be allocated.");    // No memory allocated here and list stops here 
+                break;
             }
-            else if (n = i) {
-                printf("Enter value of the %d variable "i );
-                scanf ("%d", &j); 
-                struct node n_i;
-                n_i.val = j;   
-                n_i.next = NULL;
+            else
+            {
+                printf(" Input data for node %d : ", i);   // input data for the ith element
+                scanf(" %d", &val);                        //enter input in the address of val
+                funcnode->val = val;                       // Enter value of that node
+                funcnode->next = NULL;                     // Update address of node with null 
+
+                temp-> next = funcnode;                    // Updates temp to func node and links list 
+                temp = temp->next;                         
+ 
             }
-            if (i< n; i =0, i++) {
-             printf("node n_i is: %d\n",n_i.val); 
-             printf("node n_2 is: %p\n", n_i.next);
-            }
-        }
-   }
+         }
+
+            
         
-//  ---------------------------------------------------------------------- 
-void DisplayList() {
-}
+      }
+    }
+    
+   
 
-
-
+void displayList() // displays Linked list with their elemtn
+    {
+    struct node *temp;   
+    if(stdnode == NULL)  //if no elements in list print null 
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+        temp = stdnode;     // No of elements in std node
+        while(temp != NULL)
+        {
+            printf(" Data = %d\n", temp->val);       // prints the data of current node
+            temp = temp->next;                     // advances the position of current node
+        }
+    }
+} 
 
 
