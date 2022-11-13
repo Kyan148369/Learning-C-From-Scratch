a6d28459b0c12f4a520b329535e501a79c508c97
Kyan148369 <kyanssharma10@gmail.com>
2022-11-13 05:41:05 +0530

    Rough idea of insertionsort

--- Deliverables/insertionsort3101.c
+++ Deliverables/insertionsort3101.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int inputofarray(n) {                           // n is the number of elements in the list}
+  
+  for (int j = 0; j< n-1; j++ ){                // counter to take values of each value of input array 
+    scanf (%d, &arr[j])
 
     return 0;
 }
+}
+
+int swap(min, arr[]) {                         // swap func to swap the min element with the arr[i] position its being compared to
+    temp = arr[i];
+    arr [i] = min;
+    min = temp;
+}
+
+int insertionsort(){
+int i, j, key;
+inputarray(int n);
+malloc arr = (array) malloc (sizeof(n));        // setting out space for the array in memory
+key = arr[0];
+for (int i =0; i<n-1; i++);
+    if (arr[i]<key);                        
+    swap (arr[i],key);
+    j = i-1;
+    for (i=k, i <n-1, i--);
+
+
+    return 0;
+}
+//Iterate from arr[1] to arr[N] over the array. 
+// Compare the current element (key) to its predecessor. 
+// If the key element is smaller than its predecessor, compare it to the elements before. 
+// Move the greater elements one position up to make space for the swapped element.
