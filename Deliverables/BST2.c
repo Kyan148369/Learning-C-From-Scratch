b4a6ff4b9c2c92a816b2d5e539c737530524ced2
Kyan148369 <kyanssharma10@gmail.com>
2022-11-14 03:03:52 +0530

    Finished binary search tree with array input

--- Deliverables/BST2.c
+++ Deliverables/BST2.c
@@ -3,9 +3,9 @@
 #include <assert.h>
 
 struct node {
-    int val;                                                     // val is the value we will be searching for in this binary search tree 
-    struct node * left;                                          // Everything to the left of val is smaller to it and we are creating a pointer to traverse it
-    struct node * right;                                         // Everything to the right of val is larget than it and we are creating a pointer to traverse i
+    int val;                                                    // val is the value we will be searching for in this binary search tree 
+    struct node *left;                                          // Everything to the left of val is smaller to it and we are creating a pointer to traverse it
+    struct node *right;                                         // Everything to the right of val is larget than it and we are creating a pointer to traverse i
 };
 typedef struct node* nodeAddress;                                // Basically shortcut for writing struct node all the time as node address
 
@@ -13,46 +13,54 @@ typedef struct node* nodeAddress;                                // Basically sh
 
 nodeAddress createnode(int val) {                                // allocating a new node foran  element in the binary search tree 
 
-nodeAddress node  = (nodeAddress)malloc(sizeof(nodeAddress));    // allocating memory for node 
- node->val = val;                                                // initializing val of the node
- node-> left = NULL;                                             // intializing pointer of the left pointer to NULL
- node-> right = NULL;                                            // intializing pointer of the right pointer to NULL
+nodeAddress temp  = (nodeAddress)malloc(sizeof(nodeAddress));    // allocating memory for node 
+ temp->val = val;                                                // initializing val of the node
+ temp-> left = NULL;                                             // intializing pointer of the left pointer to NULL
+ temp-> right = NULL;                                            // intializing pointer of the right pointer to NULL
  
- return (node);                                                  // return val of node
+ return temp;                                                    // return val of node
 }
 void inorder (nodeAddress root ) {                               // func for sorting it in order recursively
-    if (root != NULL ) {                                          // check to make sure that root is not NULL 
+    if (root != NULL ) {                                         // check to make sure that root is not NULL 
     inorder (root -> left);                                      // sorts inorder on the left value recursively
-    printf( "%d", root->val);                                    // next prints the val stored in root  
-    inorder (root -> right);                                       // sorts inorder on the right value recursively
+    printf( "%d \n" , root->val);                                // next prints the val stored in root  
+    inorder (root -> right);                                     // sorts inorder on the right value recursively
     }                                                                                              
 }
  
 
-nodeAddress Insertion(int val, nodeAddress root) {              // Function to insert a new element in a given binary search tree
-if (root == NULL){                                              // If no new elements present in the tree
-    return createnode(val, root);                               // calls create func to create a node address roogt and val
-}
-if  (val < root-> val) {                                        // if the val we are trying to insert is lower than the root val then insert on the left
-    root -> left = insert(val, root-> left);                    // insert val at the node address 
-    }
-elif (val > root-> val) {                                        // if the val we are trying to insert is great than the root val then insert on the right
-    root -> right = insert(val, root-> right);                   // insert val at the node address   
-    }   
-     return root;                                                // reurn the pointer address after finish
+nodeAddress Insertion(nodeAddress root, int val)                // Function to insert a new element in a given binary search tree
+{             
+    if (root == NULL)                                           // If no new elements present in the tree
+         return createnode(val);                                // calls create func to create a node with val
+
+    if  (val < root-> val)                                      // if the val we are trying to insert is lower than the root val then insert on the left
+        root -> left = Insertion(root-> left, val);             // insert val at the node address 
+    
+    else if (val > root-> val)                                   // if the val we are trying to insert is great than the root val then insert on the right
+        root -> right = Insertion(root-> right, val);            // insert val at the node address   
+       
+     return root;                                                // return the pointer address after finish
   
 }                                                         
 
 
 int main() {
-    int n
-    int inputarr[];
-    int i;
-
-    for (i =0; i<n; i++){
-        printf (" Enter value for array position" , i);
-        scanf (%d, &n);
+    int n;                                                      // initialize n data type
+    printf("Enter number of elements desired in array:");       // self explanatory
+    scanf ("%d", &n);                                           // takes n number of elements in array as input
+    int arr[n];                                                 // initializing array
+    for (int i =0; i<n; i++){                                   //takes user input for array from 1 till n
+        printf ("Enter value for array position %d: " , i);
+        scanf ("%d", &arr[i]);
     }
-    scanf("%d", )
+    nodeAddress root = NULL;                                    // initializing root address to null to empty garbage values
+    for (int i =0; i<n; i++){                          
+     root = Insertion(root, arr[i]);       
+     }                                                          // inserting elements in the array 
+	
+    inorder(root);                                              // prints out the values in order of the binary search tree 
+
     return 0;
+
 }
