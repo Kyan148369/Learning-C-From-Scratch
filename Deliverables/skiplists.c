3d5c03e2acd1745554d7e94fb009ca3a9c9eda5c
Kyan148369 <kyanssharma10@gmail.com>
2022-12-19 22:06:56 +0530

    Made some changes

--- Deliverables/skiplists.c
+++ Deliverables/skiplists.c
@@ -1,32 +1,38 @@
-#include <stdlib.h>
 #include <stdio.h>
-// over here we basically have more than 1 look ahead in the struct node and its a pointer of a pointer
+#include <stdlib.h>
+#include <time.h>
 
-struct node
-{                      // creating struct with vals and nodes, typedef helps with having
-                       // to not redeclare struct each time
-    int val;           // value stored at a node
-    struct node *link; // Pointer(points to the address) of the next node
-    struct node *lookahead1;
-    struct node *lookahead2;
-};
-typedef struct node *NodeAddress; // Renaming reference(pointer) to struct node as node address
-NodeAddress temp;
-head = (NodeAddress)malloc(sizeof(struct node));
-if (head == NULL)
-{
-    printf('no memory allocated');
-}
-else
-{
-    int val = 0;
-    printf('Enter data for node 1');
-    scanf("%d", &val);
-    head->val = val;
-    look
-        temp = head;
-}
+#define MAX_LEVEL 6
+int INT_MAX = 6;
+typedef struct node // Node structure for a skip list node
 
+{
+    int key;               // Key value of the node
+    int value;             // Value associated with the key
+    struct node **forward; // Array of pointers to the next node at each level
+} node;
+
+typedef struct skip_list
+{
+    int level;           // Current level of the skip list
+    int size;            // size of the skip list node
+    struct node *header; // Pointer to the head node
+} skip_list;
+
+skip_list *init_skip_list() // initalize skip lists
+{
+    skip_list *sl = (skip_list *)malloc(sizeof(skip_list)); // allocating memory
+    sl->level = 0;
+    sl->size = 0;
+    sl->header = (node *)malloc(sizeof(node)); // allocate memory for the head node
+    sl->header->key = INT_MAX;                 // head as max of key
+    sl->header->forward = (node **)malloc(sizeof(node *) * (MAX_LEVEL + 1));
+    for (int i = 0; i <= MAX_LEVEL; i++) // Initialize the next pointers of the head node to point to itself at each level
+    {
+        sl->header->forward[i] = NULL;
+    }
+    return sl; // Return a pointer to the newly initialized skip list
+}
 int main()
 {
     return 0;
